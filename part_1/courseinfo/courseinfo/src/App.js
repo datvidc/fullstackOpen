291fb64b8cd5758321752f4d5068d4b029bf801e
datvidc <d@vidc.dk>
2022-10-25 08:42:49 -0400

    more setup

--- part_1/courseinfo/courseinfo/src/App.js
+++ part_1/courseinfo/courseinfo/src/App.js
@@ -1,27 +1,55 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+
+  const courseList = [
+    ["part1", "Fundamentals of React", 10],
+    ["part2", "Using props to pass data", 7],
+    ["part3", "State of a component", 14],
+  ];
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course} />
+
+      <Content list={courseList} />
+      <Total list={courseList} />
     </div>
+  );
+};
+
+const Header = (props) => {
+  return (
+    <>
+      <h1> {props.name}</h1>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      {props.list.map((item) => (
+        <p key={item[0]}>
+          {item[0]} {item[1]}{" "}
+        </p>
+      ))}
+    </>
+  );
+};
+
+const Total = (props) => {
+  console.log(props.list[2][0]);
+  let numse = 0;
+  let tally = props.list.map(item => (
+    numse + item[2]; 
   )
-}
+
+
+  return (
+    <>
+      <p> total number of excersises {tally} </p>
+    </>
+  );
+};
 
 export default App
